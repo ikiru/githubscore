37dc6222928beaeb2aa0f3cf0f7fe8c3fa38581c
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-18 12:22:27 -0500

    added user and score components, and Httpservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// Modules
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+// Components
+import { AppComponent } from "./app.component";
+import { UserComponent } from "./user/user.component";
+import { ScoreComponent } from "./score/score.component";
+
+// Services
+import { HttpService } from "./http.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, UserComponent, ScoreComponent],
+  imports: [BrowserModule],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
