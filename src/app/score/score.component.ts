558378a9dc671484e72aa6a579125d0d935a4216
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-18 15:22:51 -0500

    fixed errors

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpService } from "../http.service";
 
+import { ApiService } from "../api.service";
+
 @Component({
   selector: "app-score",
   templateUrl: "./score.component.html",
   styleUrls: ["./score.component.css"]
 })
-export class ScoreComponent implements OnInit {
-  constructor(private _httpService: HttpService) {}
+export class ScoreComponent {
+  userExists = null;
+  score = null;
+  username = null;
+  promise;
+  constructor(private _apiService: ApiService) {}
 
-  ngOnInit() {}
+  calculateScore() {
+    this.promise = this._apiService.retrieveGithubUser(this.username);
+    if (this.promise) {
+      this.promise
+        .then(user => {
+          if (user.id) {
+            this.userExists = true;
+            this.score = user.public_repos + user.followers;
+          } else {
+            this.userExists = false;
+            this.score = null;
+          }
+          this.username = null;
+          console.log(this.username);
+        })
+        .catch(err => {
+          this.userExists = false;
+        });
+    } else {
+      this.userExists = false;
+    }
+  }
 }
